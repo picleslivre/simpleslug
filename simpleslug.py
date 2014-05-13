7436b6416b27556857857c82eccf8281a66a3399
Fabio Batalha <fabiobatalha@gmail.com>
2014-05-13 15:38:48 -0300

    Update simpleslug.py

--- simpleslug.py
+++ simpleslug.py
@@ -1,8 +1,6 @@
 import unicodedata
 
 __version__ = '1.0.0.dev1'
-__all__ = ['ensure', 'JsonProbe']
-
 
 def remove_accents(data):
     return ''.join(x for x in unicodedata.normalize('NFD', data) if unicodedata.category(x)[0] == 'L' or x == ' ').lower()
